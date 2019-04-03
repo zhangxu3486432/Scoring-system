a499e5bf7c4757431b7046504fd8450a5b2d22f0
zhangxu <zhangxu3486432@gmail.com>
2019-04-03 09:27:10 +0800

    前端后台bug

---
+++ frontend/src/views/CompositionList.vue
@@ -0,0 +1,53 @@
+<template>
+    <van-list
+            v-model="loading"
+            :finished="finished"
+            finished-text="没有更多了"
+    >
+        <van-cell
+                v-for="item in list"
+                :key="item.id"
+                :title="item.name"
+                :to="{name: 'JudgeView', params: { id: item.id }}"
+        />
+    </van-list>
+</template>
+
+<script>
+    export default {
+        name: "CompositionList",
+        data() {
+            return {
+                finished: true,
+                loading: false,
+                list: [
+                    {
+                        "id": 1,
+                        "number": 1,
+                        "name": "vectory智能降噪音响",
+                        "competition": 1
+                    },
+                    {
+                        "id": 2,
+                        "number": 2,
+                        "name": "智能养殖",
+                        "competition": 1
+                    },
+                    {
+                        "id": 3,
+                        "number": 3,
+                        "name": "摘椰机器人",
+                        "competition": 1
+                    },
+                ]
+            }
+        },
+        created() {
+            const id = this.$route.params.id
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
